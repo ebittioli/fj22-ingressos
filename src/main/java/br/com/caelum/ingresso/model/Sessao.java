70446bd302c0ddf23ac791395fdcc7738e46a409
Eduardo Bittioli <ebittioli@gmail.com>
2021-06-16 23:11:14 -0300

    Adicionando os Tipos de Ingressos, adicionando poster dos filmes na tela de seleção de lugares e implementando o carrinho de compras

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,36 +3,45 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 
 @Entity
 public class Sessao {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@NotNull
 	private LocalTime horario;
-	
+
 	@ManyToOne
 	private Sala sala;
-	
+
 	@ManyToOne
 	private Filme filme;
-	
+
 	private BigDecimal preco;
-	
+
+	@OneToMany(mappedBy = "sessao", fetch = FetchType.EAGER)
+	private Set<Ingresso> ingressos = new HashSet<>();
+
 	/**
-	* @deprecated hibernate only
-	*/
-	public Sessao() {		
+	 * @deprecated hibernate only
+	 */
+	public Sessao() {
 	}
 
 	public Sessao(LocalTime horario, Sala sala, Filme filme) {
@@ -80,5 +89,13 @@ public class Sessao {
 
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	public Map<String, List<Lugar>> getMapaDeLugares() {
+		return sala.getMapaDeLugares();
+	}
+
+	public boolean isDisponivel(Lugar lugarSelecionado) {
+		return ingressos.stream().map(Ingresso::getLugar).noneMatch(lugar -> lugar.equals(lugarSelecionado));
 	}
 }
